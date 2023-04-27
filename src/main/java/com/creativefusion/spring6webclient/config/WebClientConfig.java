088c4b3a6c776f8301c8e5b62db78dca340c5752
sadat.malik <sadat.malik@me.com>
2023-04-27 15:17:48 +0100

    external web client configuration

---
+++ src/main/java/com/creativefusion/spring6webclient/config/WebClientConfig.java
@@ -0,0 +1,24 @@
+package com.creativefusion.spring6webclient.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.reactive.function.client.WebClientCustomizer;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.function.client.WebClient;
+
+/**
+ * @author sm@creativefusion.net
+ */
+@Configuration
+public class WebClientConfig implements WebClientCustomizer {
+
+    private final String rootUrl;
+
+    public WebClientConfig(@Value("${webclient.rooturl}") String rootUrl) {
+        this.rootUrl = rootUrl;
+    }
+
+    @Override
+    public void customize(WebClient.Builder webClientBuilder) {
+        webClientBuilder.baseUrl(rootUrl);
+    }
+}
